e5a95cf52f2e54f6d4b0dca601552770b50d14a6
Cameron Scott <camscottgreen@hotmail.com>
2019-09-16 22:20:36 +1000

    Fixed cipher wraparound logic

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'sinatra/reloader'
+require_relative 'caesar-cipher.rb'
 
 get '/' do
+    if params['to_cipher'] && params['shift']
+        @ciphered = caesar_cipher(params['to_cipher'],params['shift'].to_i)
+    end
     erb :index
 end
--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,22 +1,22 @@
 def caesar_cipher(string, shift)
-   chars = string.chars
-   ciphertext = ""
-   chars.each do |c|
-        if c.match(/[a-z]/)
-            val = c.ord + shift
-            if val > "z".ord
-            val = "a".ord + ((val - "a".ord) % 26)
-        end
-        ciphertext += val.chr
-        elsif c.match(/[A-Z]/)
-            val = c.ord + shift
-            if val > "Z".ord
-                val = "A".ord + ((val - "A".ord) % 26)   
-            end
-        ciphertext += val.chr
-        else
-        ciphertext += c
-        end
-    end
-    return ciphertext
-end
+    chars = string.chars
+    ciphertext = ""
+    chars.each do |c|
+         if c.match(/[a-z]/)
+             val = c.ord + shift
+             if val < "a".ord || val > "z".ord
+                 val = "a".ord + ((val - "a".ord) % 26)
+             end
+             ciphertext += val.chr
+         elsif c.match(/[A-Z]/)
+             val = c.ord + shift
+             if val > "Z".ord || val < "A".ord
+                 val = "A".ord + ((val - "A".ord) % 26)
+             end
+             ciphertext += val.chr
+         else
+             ciphertext += c
+         end
+     end
+     return ciphertext
+ end
